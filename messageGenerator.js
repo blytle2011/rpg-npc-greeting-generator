0f99c47d4ffc03ae1e3855724cca4ca90d4bfebd
blytle2011 <blytle2011@gmail.com>
2025-11-12 13:56:09 -0500

    Update messageGenerator.js

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,6 +1,7 @@
 function friendlyGreeting(arr, arr, arr) {
-const messPart1 = ['', '', '', '', ''];
-const messPart2 = ['', '', '', '', ''];
-const messPart3 = ['', '', '', '', ''];
-let greeting = (messPart1[Math.floor(Math.random() * messPart1.length)]) + ',' + (messPart2[Math.floor(Math.random() * messPart2.length)]) + '.' + (messPart3[Math.floor(Math.random() * messPart3.length)]) + '.';
+const messPart1 = ['Greetings', 'Hello', 'Well Met', 'Hail', 'Salutations'];
+const messPart2 = ['Adventurer', 'Traveler', 'Friend', 'Hero', 'Warrior'];
+const messPart3 = ['How so you fare?', 'Good to see you.', 'Are you well?', '', ''];
+let greeting = (messPart1[Math.floor(Math.random() * messPart1.length)]) + ',' + (messPart2[Math.floor(Math.random() * messPart2.length)]) + '.' + (messPart3[Math.floor(Math.random() * messPart3.length)]);
+alert(greeting);
 }
