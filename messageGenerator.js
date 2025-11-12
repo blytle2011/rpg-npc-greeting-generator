763bbc5fc2c194f9bd5d71d45fbe0278950b7ec6
blytle2011 <blytle2011@gmail.com>
2025-11-12 13:29:58 -0500

    Create messageGenerator.js

---
+++ messageGenerator.js
@@ -0,0 +1,6 @@
+function friendlyGreeting(arr, arr, arr) {
+const messPart1 = ['', '', '', '', ''];
+const messPart2 = ['', '', '', '', ''];
+const messPart3 = ['', '', '', '', ''];
+let greeting = (messPart1[Math.floor(Math.random() * messPart1.length)]) + ',' + (messPart2[Math.floor(Math.random() * messPart2.length)]) + '.' + (messPart3[Math.floor(Math.random() * messPart3.length)]) + '.';
+}
