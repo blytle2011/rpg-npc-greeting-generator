aa45e6d000231ab5a50bd3cd59fff341cca41959
blytle2011 <blytle2011@gmail.com>
2025-11-12 14:52:07 -0500

    HTML file to run JS file. Edited the final output to enter missing spaces.

--- messageGenerator.js
+++ messageGenerator.js
@@ -3,7 +3,7 @@ const mess2 = ['Adventurer', 'Traveler', 'Friend', 'Hero', 'Warrior'];
 const mess3 = ['How do you fare?', 'Good to see you.', 'Are you well?', 'Do you need anything?', 'How can I be of assistance?'];
 
 function friendlyGreeting(arr1, arr2, arr3) {
-let greeting = (arr1[Math.floor(Math.random() * arr1.length)]) + ',' + (arr2[Math.floor(Math.random() * arr2.length)]) + '.' + (arr3[Math.floor(Math.random() * arr3.length)]);
+let greeting = (arr1[Math.floor(Math.random() * arr1.length)]) + ', ' + (arr2[Math.floor(Math.random() * arr2.length)]) + '. ' + (arr3[Math.floor(Math.random() * arr3.length)]);
 alert(greeting);
 }
 
